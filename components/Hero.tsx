cfe6d3f35d749c829ceb511298b3fcf06c517b9d
fuad.herac@gmail.com <fuad.herac@gmail.com>
2023-11-25 22:06:50 +0100

    hero image alt

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export const Hero = () => {
   return (
     <section className="max-container padding-container flex items-center h-[calc(100vh-67px)]">
-      <Image src="/hero-image.avif" width="1400" height="300" priority={true} className="ml-[-480px] mb-[150px]" />
+      <Image src="/hero-image.avif" alt="motorcycle image" width="1400" height="300" priority={true} className="ml-[-480px] mb-[150px]" />
       <div className="text-black-90">
         <h1 className="text-[40px] font-bold">SEARCH OR COMPARE</h1>
         <div className="w-[400px] text-[19px] font-light">
