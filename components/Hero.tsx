62569fc4e8d9c4e8457f2ea539d784020fae9fb5
fuad.herac@gmail.com <fuad.herac@gmail.com>
2023-11-25 22:15:01 +0100

    hero paragraph

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,11 +4,11 @@ export const Hero = () => {
   return (
     <section className="max-container padding-container flex items-center h-[calc(100vh-67px)]">
       <Image src="/hero-image.avif" alt="motorcycle image" width="1400" height="300" priority={true} className="ml-[-480px] mb-[150px]" />
-      <div className="text-black-90">
+      <div className="text-black-90 md:ml-[-35px]">
         <h1 className="text-[40px] font-bold">SEARCH OR COMPARE</h1>
-        <div className="w-[400px] text-[19px] font-light">
+        <p className="w-[400px] text-[19px] font-light">
           Discover information about motorcycles or compare two motorcycles on the rhace website or mobile app.
-        </div>
+        </p>
       </div>
     </section>
   )
