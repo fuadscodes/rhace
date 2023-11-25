7d016d6fd3d5e7f3894dd0e8c625564c866165bf
fuad.herac@gmail.com <fuad.herac@gmail.com>
2023-11-25 22:51:25 +0100

    responsive Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,11 +2,18 @@ import Image from "next/image"
 
 export const Hero = () => {
   return (
-    <section className="max-container padding-container flex items-center h-[calc(100vh-67px)]">
-      <Image src="/hero-image.avif" alt="motorcycle image" width="1400" height="300" priority={true} className="ml-[-480px] mb-[150px]" />
-      <div className="text-black-90 md:ml-[-35px]">
+    <section className="max-container padding-container flex-co xl:flex items-center xl:h-[calc(100vh-67px)]">
+      <Image 
+        priority={true} 
+        src="/hero-image.avif" 
+        alt="motorcycle image" 
+        width="1400" 
+        height="300" 
+        className="xl:ml-[-450px] xl:mb-[150px]" 
+      />
+      <div className="text-black-90 xl:ml-[-35px] text-center xl:text-left p-[64px] xl:p-0">
         <h1 className="text-[40px] font-bold">SEARCH OR COMPARE</h1>
-        <p className="w-[400px] text-[19px] font-light">
+        <p className="w-full xl:w-[400px] text-[19px] font-light">
           Discover information about motorcycles or compare two motorcycles on the rhace website or mobile app.
         </p>
       </div>
