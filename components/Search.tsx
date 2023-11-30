e833c241342c8610696ee4b2395179cd54d1e2f2
fuad.herac@gmail.com <fuad.herac@gmail.com>
2023-12-01 00:09:12 +0100

    align items on search section

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import useGetMotorcycle from "../src/util/useGetMotorcycle";
+import { Motorcycle } from "./Motorcycle";
 
 export const Search = () => {
   const { motorcycle, loading, fetchData } = useGetMotorcycle();
@@ -12,25 +13,26 @@ export const Search = () => {
   return (
     <div
       id="search"
-      className="bg-black-50 text-white shadow-lg py-[64px] md:py-[120px] px-[32px] scroll-mt-[67px] flex-col justify-center h-[600px]"
+      className="bg-black-50 text-white shadow-lg py-[64px] md:py-[120px] px-[32px] scroll-mt-[67px] flex-col min-h-[600px]"
     >
-      <h2 className="font-bold text-[24px] md:text-[32px] text-center">
+      <h2 className="font-bold text-[24px] md:text-[32px] text-center mb-[40px]">
         SEARCH
       </h2>
 
-      {!motorcycle && !loading && (
-        <div
-          className="cursor-pointer"
-          onClick={() => {
-            handleOnClick();
-          }}
-        >
-          Search
-        </div>
-      )}
-      {loading && <div>Loading...</div>}
-      <div className="w-full flex flex-col items-center justify-center"></div>
-      <div>{motorcycle?.make}</div>
+      <div className="flex items-center justify-center">
+        {!motorcycle && !loading && (
+          <div
+            className="cursor-pointer w-min"
+            onClick={() => {
+              handleOnClick();
+            }}
+          >
+            Start
+          </div>
+        )}
+        {loading && <div>Loading...</div>}
+        <Motorcycle data={motorcycle} />
+      </div>
     </div>
   );
 };
