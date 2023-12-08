5449d983d55ee885b420064b40a261f937fa4927
fuad.herac@gmail.com <fuad.herac@gmail.com>
2023-12-08 15:54:35 +0100

    minor change

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -2,7 +2,7 @@ export const GetApp = () => {
   return (
     <div
       id="get-app"
-      className="py-[64px] md:py-[120px] px-[32px] scroll-mt-[72px] bg-gray-10"
+      className="py-[64px] md:py-[120px] px-[32px] scroll-mt-[72px]"
     >
       Get App
     </div>
