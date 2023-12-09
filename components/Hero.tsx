9b92adb980a1bc269e4d9d707f2ee074fdb0a6ba
fuad.herac@gmail.com <fuad.herac@gmail.com>
2023-12-09 19:27:54 +0100

    hero section on large screen

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,9 +7,17 @@ export const Hero = () => {
         priority={true}
         src="/hero-image.avif"
         alt="motorcycle image"
-        width="1400"
+        width="1550"
         height="300"
-        className="xl:ml-[-450px] xl:mb-[150px]"
+        className="xl:ml-[-450px] xl:mb-[150px] 2xl:hidden"
+      />
+      <Image
+        priority={true}
+        src="/hero-image.avif"
+        alt="motorcycle image"
+        width="4500"
+        height="1550"
+        className="hidden 2xl:block 2xl:ml-[-800px] 2xl:mb-[250px]"
       />
       <div className="text-black-90 xl:ml-[-100px] text-center xl:text-left px-[32px] py-[64px] xl:p-0">
         <h1 className="text-[32px] md:text-[40px] font-bold">
