d22d00fae52f9224942ff1b4b55b37e3754e1974
fuad.herac@gmail.com <fuad.herac@gmail.com>
2023-12-09 18:21:33 +0100

    MotorcycleProps interface

--- components/Motorcycle.tsx
+++ components/Motorcycle.tsx
@@ -1,7 +1,49 @@
 "use client";
 import React from "react";
 
-export const Motorcycle = ({ data }) => {
+interface MotorcycleProps {
+  data: {
+    make: string;
+    model: string;
+    year: string;
+    type: string;
+    displacement: string;
+    engine: string;
+    power: string;
+    torque: string;
+    compression: string;
+    bore_stroke: string;
+    valves_per_cylinder: string;
+    fuel_system: string;
+    fuel_control: string;
+    ignition: string;
+    lubrication: string;
+    cooling: string;
+    gearbox: string;
+    transmission: string;
+    clutch: string;
+    frame: string;
+    front_suspension: string;
+    front_wheel_travel: string;
+    rear_suspension: string;
+    rear_wheel_travel: string;
+    front_tire: string;
+    rear_tire: string;
+    front_brakes: string;
+    rear_brakes: string;
+    total_weight: string;
+    seat_height: string;
+    total_height: string;
+    total_length: string;
+    total_width: string;
+    ground_clearance: string;
+    wheelbase: string;
+    fuel_capacity: string;
+    starter: string;
+  };
+}
+
+export const Motorcycle: React.FC<MotorcycleProps> = ({ data }) => {
   if (!data) return null;
 
   const originalKeys: string[] = Object.keys(data);
@@ -17,7 +59,9 @@ export const Motorcycle = ({ data }) => {
           <div key={index} className="mb-4 text-[16px]">
             <div className="mb-1">
               <span className="font-bold">{mappedKey}: </span>
-              <span className="font-light">{data[originalKey]}</span>
+              <span className="font-light">
+                {data[originalKey as keyof typeof data]}
+              </span>
             </div>
           </div>
         );
